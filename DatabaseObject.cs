34b3a30f7d08077b52ac75d78673381aaa9bc148
tiltfire <66975412+tiltfire@users.noreply.github.com>
2022-08-11 12:18:55 +0300

    write .csv file contents to database

---
+++ DatabaseObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SQLite;
+using SolitaAssignment.Models;
+
+namespace SolitaAssignment
+{
+    public class DatabaseObject
+    {
+        SQLiteConnection? connection;
+
+        public void Initsql()
+        {
+            connection = new SQLiteConnection("Data Source=database.db;Version=3");
+            connection.Open();
+        }
+
+        public void ClearDataBase()
+        {
+            string order = "DELETE FROM bikejourneys;";
+            SQLiteCommand command = new SQLiteCommand(order,connection);
+            command.ExecuteNonQuery();
+        }
+        public void InsertBikeJourneys(List<BikeJourney> Journeys)
+        {
+            string order = "BEGIN TRANSACTION;";
+            int skips = 0;
+            foreach(BikeJourney Journey in Journeys)
+            {
+                if(int.TryParse(Journey.duration, out int dur))
+                {
+                    if(dur < 10)
+                    {
+                        continue;
+                    }
+                }
+                if(int.TryParse(Journey.covereddistance,out int dist))
+                {
+                    if(dist < 10)
+                    {
+                        continue;
+                    }
+                }
+                order += $"INSERT INTO bikejourneys VALUES ('{Journey.departuretime}','{Journey.returntime}','{Journey.departurestationid}','{Journey.departurestationname}','{Journey.returnstationid}','{Journey.returnstationname}','{Journey.covereddistance}','{Journey.duration}');";
+            }
+            order += "COMMIT;";
+            SQLiteCommand command = new SQLiteCommand(order, connection);
+            command.ExecuteNonQuery();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,87 @@
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+using System.Linq;
+using SolitaAssignment.Models;
 
 namespace SolitaAssignment
 {
     public class SolitaAssignment
     {
+        public static DatabaseObject DBobject = new DatabaseObject();
+        public static void CSVintoDB(string path)
+        {
+            using(var reader = new StreamReader(path))
+            {
+                reader.ReadLine(); //skip first line of CSV file
+                List<BikeJourney> Journeys = new List<BikeJourney>();
+                int i = 0;
+                int set = 1; //used to write to console how many sets of journeys added to database
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    BikeJourney Journey = new BikeJourney();
+                    Journey.departuretime = values[0];
+                    Journey.returntime = values[1];
+                    Journey.departurestationid = values[2];
+                    Journey.departurestationname = values[3];
+                    Journey.returnstationid = values[4];
+                    Journey.returnstationname = values[5];
+                    Journey.covereddistance = values[6];
+                    Journey.duration = values[7];
+                    if(i < 1000)
+                    {
+                        Journeys.Add(Journey);
+                        i++;
+                    }
+                    else
+                    {
+                        DBobject.InsertBikeJourneys(Journeys);
+                        Journeys.Clear();
+                        i = 0;
+                        Console.WriteLine("Set " + set + " added to database from " + path);
+                        set++;
+                    }
+                }
+            }
+        }
+        public static void readCSVs()
+        {
+            string[] filepaths = Directory.GetFiles("./");
+            foreach(string filepath in filepaths)
+            {
+                if (filepath.EndsWith(".csv"))
+                {
+                    Console.WriteLine("Write 'erase' to clear database contents.");
+                    Console.WriteLine($"Write 'read' to read contents from {filepath} and write them to the database.");
+                    Console.WriteLine("Press enter to continue.");
+                    string answer = Console.ReadLine();
+                    if(answer == "read")
+                    {
+                        CSVintoDB(filepath);
+                    }
+                    else if(answer == "erase")
+                    {
+                        DBobject.ClearDataBase();
+                    }
+                    //Console.WriteLine(filepath);
+                }
+            }
+        }
         public static void Main(string[] args)
         {
+            DBobject.Initsql();
+            readCSVs();
 
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //builder.Services.AddControllers();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
 
             app.Run();
         }
