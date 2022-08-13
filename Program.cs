3a647dc7b3258e6e6d296c35c157ce4b2d3ce545
tiltfire <66975412+tiltfire@users.noreply.github.com>
2022-08-13 11:17:06 +0300

    erase quotes and commas from CSV entries

--- Program.cs
+++ Program.cs
@@ -9,24 +9,55 @@ namespace SolitaAssignment
     public class SolitaAssignment
     {
         public static DatabaseObject DBobject = new DatabaseObject();
+        public static string EraseQuotes(string line)
+        {
+            string newline = "";
+            bool withinQuote = false;
+            foreach(char c in line)
+            {
+                if (c == '"' && !withinQuote)
+                    withinQuote = true;
+                else if (c == '"' && withinQuote)
+                    withinQuote = false;
+
+                if (withinQuote && c != ',' && c != '"')
+                    newline += c;
+
+                if(!withinQuote && c != '"')
+                    newline += c;
+
+            }
+            //some entries in the CSV files are quoted because they contain commas.
+            //trying to add such entries into the database turned out messy, so I wrote this loop which erases quotes and commas that are within quotes.
+
+            return newline;
+        }
         public static void CSVintoDB(string path, bool isJourneyFile)
         {
             int lineCount = File.ReadLines(path).Count();
 
             using(var reader = new StreamReader(path))
             {
-                //reader.ReadLine(); //skip first line of CSV file
+                reader.ReadLine(); //skip first line of CSV file
 
                 List<BikeJourney> Journeys = new List<BikeJourney>();
                 List<BikeStation> Stations = new List<BikeStation>();
 
                 int i = 0;
-                int linePosition = 1;
+                int linePosition = 2;
                 int set = 1; //used to write to console how many sets of journeys added to database
 
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
+
+                    if (line.Contains("\""))
+                    {
+                        //Console.WriteLine();
+                        //Console.WriteLine(line);
+                        line = EraseQuotes(line);
+                        //Console.WriteLine(line);
+                    }
 
                     var values = line.Split(',');
 
@@ -75,6 +106,7 @@ namespace SolitaAssignment
                     Console.WriteLine($"Write 'journey' to read contents from {filepath} and write them to the bikejourneys table.");
                     Console.WriteLine($"Write 'station' to read contents from {filepath} and write them to the bikestations table.");
                     Console.WriteLine("Press enter to continue.");
+
                     string answer = Console.ReadLine();
                     if(answer == "journey")
                     {
